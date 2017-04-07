05dabe75f62b960e83296c62736beb2e5915ce20
Liu Mingyang <e.d.boy@aliyun.com>
2017-04-07 23:37:46 +0800

    ADD LICENSE

--- terminator.hpp
+++ terminator.hpp
@@ -1,3 +1,5 @@
+// Liu M , 2017.4.6
+
 #pragma once
 
 #include "token.hpp"
@@ -61,3 +63,21 @@ public /*function*/:
 		return token::get_value<string>();
 	}
 };
+
+
+
+//Copyright 2017 Liu Mingyang.
+//This source file is part of SimpleParser.
+
+//SimpleParser is free software : you can redistribute it and / or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//SimpleParser is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with SimpleParser.If not, see <http://www.gnu.org/licenses/>.
